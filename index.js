fc0f6038d4500f2fb8b536c0b7b99905f92be365
Seehait Chockthanyawat <s.chockthanyawat@gmail.com>
2021-08-15 14:06:33 +0700

    validate response from the server

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
 const puppeteer = require('puppeteer')
 
+const HOST = 'https://popcat.click'
+const POP_ENDPOINT = 'https://stats.popcat.click/pop?pop_count='
 const SECONDS_IN_MS = 1000
-const REPORT_INTERVAL_IN_S = 1
 const COUNTER_SELECTOR = '.counter'
 
 const execute = async () => {
     const browser = await puppeteer.launch()
-
     try {
+        let totalPops = 0
+
         const page = await browser.newPage()
         page.setUserAgent('Mozilla/5.0 (Macintosh Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73')
+        page.on('requestfinished', async (request) => {
+            if (!request.url().startsWith(POP_ENDPOINT)) {
+                return
+            }
 
-        await page.goto('https://popcat.click')
-        await page.waitForSelector(COUNTER_SELECTOR)
-        await page.evaluate(function () {
-            const EVENT = new KeyboardEvent('keydown', { key: 'SPACE' })
-            const EXPECTED_CPS = 800 / 30
-            const BATCH_SIZE = 1
-
-            const interval = (1000 * BATCH_SIZE) / EXPECTED_CPS
-            setInterval(() => {
-                for (let i = 0; i < BATCH_SIZE; i++) {
-                    document.dispatchEvent(EVENT)
-                }
-            }, interval)
+            const responseStatus = (await request.response()).status()
+            if (responseStatus == 201) {
+                const popCount = parseInt(/^[^\d]*(\d+)/.exec(request.url())[1])
+                totalPops += popCount
+                console.info(`Pop count=${popCount} is updated successfully! Total pops=${totalPops}`)
+            } else {
+                console.warn(`Pop count is not updated, got ${responseStatus}`)
+            }
         })
 
-        let previousCnt = 0.0
-        let currentCnt = 0.0
-        let timeConsumed = 0
+        await page.goto(HOST)
+        await page.waitForSelector(COUNTER_SELECTOR)
+        await page.evaluate(function () {
+            const KEY_DOWN_EVENT = new KeyboardEvent('keydown', { key: 'a' })
+            const KEY_UP_EVENT = new KeyboardEvent('keyup', { key: 'a' })
+            const INTERVAL = 30
+
+            setInterval(() => {
+                document.dispatchEvent(KEY_DOWN_EVENT)
+                document.dispatchEvent(KEY_UP_EVENT)
+            }, INTERVAL)
+        })
+
+        console.info('started!')
+
         while (true) {
-            const element = await page.$(COUNTER_SELECTOR)
-            currentCnt = parseFloat((await page.evaluate(el => el.textContent, element)).replace(/\,/g, '')) || 0.0
-
-            console.log(`Total: ${currentCnt} clicks, current rate: ${((currentCnt - previousCnt) / REPORT_INTERVAL_IN_S).toFixed(2)} cps, average rate: ${(currentCnt / timeConsumed).toFixed(2)} cps`)
-            previousCnt = currentCnt
-            timeConsumed += REPORT_INTERVAL_IN_S
-
-            await new Promise(res => setTimeout(res, SECONDS_IN_MS * REPORT_INTERVAL_IN_S))
+            await new Promise(res => setTimeout(res, SECONDS_IN_MS))
         }
+    } catch (error) {
+        console.error(`${error}`)
     } finally {
         await browser.close()
+        console.info('existed!')
     }
 }
 
